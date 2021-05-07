6f65dc6af5c088df61358d68515bccde620d3a08
kwon, minho <minhob38@gmail.com>
2021-05-07 16:26:12 +0900

    Add image on login screen

--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import CustomButton from "../components/CustomButton";
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
+      <Image style={styles.image} source={require("../assets/soccerBall.jpeg")} />
       <CustomButton title="Google로 시작하기" />
     </View>
   );
@@ -14,7 +15,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "space-around",
     backgroundColor: "#FAF2E0",
+  },
+  image: {
+    height: "50%",
+    width: "50%",
+    resizeMode: "cover",
   },
 });
