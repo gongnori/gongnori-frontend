3a8af5bf3db08bcc3f2f3d01173e9fdabe786ef0
kwon, minho <minhob38@gmail.com>
2021-05-08 17:59:25 +0900

    Connect server

--- actions/actions.js
+++ actions/actions.js
@@ -1,15 +1,19 @@
-const authLogin = (accessToken) => async (dispatch) => {
-  const response = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
-    method: "GET",
-    headers: { Authorization: `Bearer ${accessToken}` },
+const authLogin = (userInfo) => async (dispatch) => {
+  console.log("!!!");
+  console.log(userInfo);
+
+  const response = await fetch("http://192.168.0.6:8000/auth/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ userInfo }),
   });
 
-  const userinfo = await response.json();
+  // const userinfo = await response.json();
 
-  dispatch({
-    type: "AUTH_LOGIN",
-    payload: userinfo,
-  });
-}
+  // dispatch({
+  //   type: "AUTH_LOGIN",
+  //   payload: userinfo,
+  // });
+};
 
 export { authLogin };
--- screens/LoginScreen.jsx
+++ screens/LoginScreen.jsx
@@ -1,35 +1,41 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { StyleSheet, View, Image } from "react-native";
 import { useDispatch } from "react-redux";
 import * as Google from "expo-auth-session/providers/google";
 import CLIENT_ID from "../config/auth";
 import CustomButton from "../components/CustomButton";
-import { authLogin } from "../actions/actions"
+import { authLogin } from "../actions/actions";
 
 export default function LoginScreen() {
   const dispatch = useDispatch();
-
   const [request, response, promptAsync] = Google.useAuthRequest({
     expoClientId: CLIENT_ID.expo,
     webClientId: CLIENT_ID.web,
-    scopes: ["profile", "email"],
   });
 
   useEffect(() => {
-    if (response?.type === "success") {
-      dispatch(authLogin(response.authentication.accessToken));
-    }
+    const getGoogleUseInfo = async () => {
+      if (response?.type === "success") {
+        const accessToken = response.authentication.accessToken;
+        const res = await fetch("https://www.googleapis.com/oauth2/v2/userinfo", {
+          method: "GET",
+          headers: { "Authorization": `Bearer ${accessToken}` },
+        });
+  
+        const userInfo = await res.json();
+        dispatch(authLogin(userInfo));
+      }
+    };
+
+    getGoogleUseInfo();
   }, [response]);
 
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={require("../assets/soccerBall.jpeg")} />
-      <CustomButton title="Google로 시작하기" onPress={() => console.log("!!")} />
       <CustomButton
-        title="Login"
-        onPress={() => {
-          promptAsync();
-        }}
+        title="Google로 시작하기"
+        onPress={() => promptAsync()}
       />
     </View>
   );
